aa41b975ffe0f3bdc105d29d67a3877bbf0ed952
Evans Manyala <evans.manyala@gmail.com>
2023-12-14 23:49:37 +0300

    Adding header file details

--- monty.h
+++ monty.h
@@ -34,4 +34,8 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void push(stack_t **stack, int value, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void free_stack(stack_t *stack);
+
 #endif
