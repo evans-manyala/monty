63e7f7c8f218f51bc4645981d0bfe4ff94767a22
Evans Manyala <evans.manyala@gmail.com>
2023-12-15 13:33:03 +0300

    Adding error fixes

--- monty.c
+++ monty.c
@@ -1,5 +1,10 @@
 #include "monty.h"
 
+/**
+ * push_instruction - Pushes an element onto the stack.
+ * @stack: Pointer to the stack.
+ * @lineNum: Line number in the Monty file.
+ */
 void push_instruction(stack_t **stack, unsigned int lineNum)
 {
 	char argument[100];
@@ -12,25 +17,32 @@ void push_instruction(stack_t **stack, unsigned int lineNum)
 
 	int value;
 
-	if (sscanf(argument, "%d", &value) != 1)
+	if (kstrtol(argument, 0, 10, &value) != 0)
 	{
 		fprintf(stderr, "Error: L%u: Invalid argument '%s' for 'push'\n",
-		lineNum, argument);
+			lineNum, argument);
 		exit(1);
 	}
 
 	stack_t *newNode = malloc(sizeof(stack_t));
+
 	newNode->n = value;
 	newNode->prev = NULL;
 	newNode->next = *stack;
 	*stack = newNode;
 }
 
+/**
+ * pall_instruction - Prints all values in the stack.
+ * @stack: Pointer to the stack.
+ * @lineNum: Line number in the Monty file.
+ */
 void pall_instruction(stack_t **stack, unsigned int lineNum)
 {
 	if (*stack == NULL)
 		return;
 	stack_t *current = *stack;
+
 	while (current)
 	{
 		printf("%d\n", current->n);
