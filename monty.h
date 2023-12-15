538dbc96357e232ecdd898af05f73d5e8ffe429d
evans-manyala <evans.manyala@gmail.com>
2023-12-15 12:37:32 +0300

    Adding header file

--- monty.h
+++ monty.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -33,6 +34,10 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+typedef void (*instruction_func)(stack_t **stack, unsigned int line_number);
+
+instruction_func instructions[] = { push_instruction, pall_instruction,};
 
 void push(stack_t **stack, int value, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
